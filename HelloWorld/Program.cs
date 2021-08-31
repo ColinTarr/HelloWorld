8c56dfb36a079146758fac5cdb9f7548fa9f0fa2
tarr_colin <tarr_colin@G-02-07>
2021-08-31 09:54:49 -0400

    Add project files.

---
+++ HelloWorld/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace HelloWorld
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World");
+            
+            Console.WriteLine("How old are you?");
+            string input = Console.ReadLine();
+            Console.WriteLine($"You are {input} years old!");
+
+            //These are some comments
+            /*These are some 
+                multi-line comments.*/
+        }
+    }
+}
